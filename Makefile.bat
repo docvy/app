cce8a4c87a31004c2117c8bf7f0af5a27034dc3c
GochoMugo <mugo@forfuture.co.ke>
2015-03-01 17:30:50 +0300

    Add batch script to emulate Makefile

---
+++ Makefile.bat
@@ -0,0 +1,44 @@
+@ECHO OFF
+
+:args
+IF NOT "%1"=="" (
+  call :%1
+  SHIFT
+  GOTO args
+)
+GOTO :EOF
+
+:releases
+goto build
+grunt
+
+:build
+cp -R src/* build
+call :build_server
+call :build_viewer
+cd build
+npm install
+cd ..
+
+
+:build_server
+npm install docvy-server
+mv node_modules/docvy-server build/server
+
+
+:build_viewer
+npm install docvy-viewer
+mv node_modules/docvy-viewer build/viewer
+
+
+:src
+call :src_viewer
+call :src_server
+
+
+:src_viewer
+mklink src\viewer ..\..\docvy-viewer\dist
+
+
+:src_server
+mklink /d src\server ..\..\docvy-server
